ac0f2e553d3509f6ce2435935ef4b049151b553b
CameliaMitran <36982747+CameliaMitran@users.noreply.github.com>
2018-04-23 17:12:16 +0300

    Add files via upload

---
+++ ProdConsMutex.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading;
+
+namespace ProducerConsumer
+{
+    public class ProdConsMutex
+    {
+        private int bufferSize;
+        private int nrOfProducts;
+        
+
+        public ProdConsMutex(int bufferSize, int nrOfProducts) {
+            this.nrOfProducts = nrOfProducts;
+            this.bufferSize = bufferSize;
+        }
+
+     
+        int Available = 0;
+        Mutex BufferIsFull = new Mutex(true);
+        Mutex BufferIsEmpty = new Mutex(true);
+        Mutex BufferLock = new Mutex(false);
+
+
+        public void Produce()
+        {
+
+            for(int i = 0; i < nrOfProducts; i++ )
+            {
+                while(Available == bufferSize)
+                {
+                    Console.WriteLine("Producer wait ");
+                    BufferIsFull.WaitOne(2000);
+                }
+
+                BufferLock.WaitOne();
+                Available++;
+                Console.WriteLine("Produced");
+                BufferLock.ReleaseMutex();
+
+                try
+                {
+                    BufferIsEmpty.ReleaseMutex();
+                }
+                catch { }
+
+
+            }
+
+        }
+
+        public void Consume()
+        {
+            for (int i = 0; i < nrOfProducts; i++)
+            {
+                while (Available < 1)
+                {
+                    Console.WriteLine("Consumer wait ");
+                    BufferIsEmpty.WaitOne(1000);
+                }
+
+                BufferLock.WaitOne();
+                Available--;
+                Console.WriteLine("Consumed");
+                BufferLock.ReleaseMutex();
+
+                try
+                {
+                    BufferIsFull.ReleaseMutex();
+                }
+                catch { }
+
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ProducerConsumer
+{
+    class Program
+    {
+
+        private const int nrOfProducts = 100;
+        private const int BufferSize = 20;
+
+
+        static void Main(string[] args)
+        {
+            var prodCons = new ProdConsMutex(BufferSize, nrOfProducts);
+            Thread prod = new Thread(new ThreadStart(prodCons.Produce));
+            Thread cons = new Thread(new ThreadStart(prodCons.Consume));
+            prod.Start();
+            cons.Start();
+
+            prod.Join();
+            cons.Join();
+           
+        }
+    }
+}
